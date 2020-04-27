a120fdae4c3783fb9c31e2c5d6f6bc031d9930d4
RicardoWorf <steve4137@gmail.com>
2020-04-27 20:56:26 -0300

    Add export dependencie cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const api = express();
 
+api.use(cors());
 api.use(express.json());
 api.use(routes);
+
 
 api.listen(3333);
